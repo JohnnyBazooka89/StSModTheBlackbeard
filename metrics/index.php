d07ab53aa112c8eb42142d5aa9aa9e1fa8ad5f62
JohnnyBazooka89 <43926043+JohnnyBazooka89@users.noreply.github.com>
2019-01-03 18:25:30 +0100

    Update index.php

--- metrics/index.php
+++ metrics/index.php
@@ -4,7 +4,7 @@ This page is the destination for metric uploads.
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $json = file_get_contents('php://input');
 
-    $directory = 'run';
+    $directory = 'run/';
  
     if(!is_dir($directory)){
         mkdir($directory, 0751, true);
