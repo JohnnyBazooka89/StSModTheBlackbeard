379b3104f9ace0f1c35a9b6032d3f76b4700ccd7
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2019-02-09 14:42:27 +0100

    v. 1.7.15 - Localized Humongous Cannonball.

--- src/main/java/blackbeard/cards/HumongousCannonball.java
+++ src/main/java/blackbeard/cards/HumongousCannonball.java
@@ -9,12 +9,16 @@ import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class HumongousCannonball extends CustomCard {
     public static final String ID = "blackbeard:HumongousCannonball";
+    public static final String DEFAULT_IMAGE_ID = "HumongousCannonballENG";
+    public static final String POLISH_IMAGE_ID = "HumongousCannonballPOL";
+    public static final String RUSSIAN_IMAGE_ID = "HumongousCannonballRUS";
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
@@ -23,7 +27,7 @@ public class HumongousCannonball extends CustomCard {
     private static final int UPGRADE_PLUS_ATTACK_DMG = 12;
 
     public HumongousCannonball() {
-        super(ID, NAME, TheBlackbeardMod.getCardImagePath(ID), COST, DESCRIPTION, CardType.ATTACK,
+        super(ID, NAME, getCardImagePathBasedOnUserLanguage(), COST, DESCRIPTION, CardType.ATTACK,
                 AbstractCardEnum.BLACKBEARD_BLACK, CardRarity.RARE, CardTarget.ENEMY);
 
         this.baseDamage = this.damage = ATTACK_DMG;
@@ -44,5 +48,16 @@ public class HumongousCannonball extends CustomCard {
             this.upgradeName();
             this.upgradeDamage(UPGRADE_PLUS_ATTACK_DMG);
         }
+    }
+
+    private static String getCardImagePathBasedOnUserLanguage() {
+        switch (Settings.language) {
+            case POL:
+                return TheBlackbeardMod.getCardImagePath(POLISH_IMAGE_ID);
+            case RUS:
+                return TheBlackbeardMod.getCardImagePath(RUSSIAN_IMAGE_ID);
+        }
+
+        return TheBlackbeardMod.getCardImagePath(DEFAULT_IMAGE_ID);
     }
 }
