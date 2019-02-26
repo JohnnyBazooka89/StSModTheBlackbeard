91f89872cafdf820b68bd6acc8e6d6ee595270b5
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2019-02-26 19:56:46 +0100

    v.1.9.2 - Bug fixes

--- src/main/java/blackbeard/patches/BlackbeardExclusiveEventsPatches.java
+++ src/main/java/blackbeard/patches/BlackbeardExclusiveEventsPatches.java
@@ -1,30 +1,43 @@
 package blackbeard.patches;
 
 import blackbeard.enums.TheBlackbeardEnum;
+import blackbeard.events.SsssserpentBlackbeardEvent;
+import blackbeard.events.VampiresBlackbeardEvent;
+import javassist.CtBehavior;
+
+import com.evacipated.cardcrawl.modthespire.lib.LineFinder;
+import com.evacipated.cardcrawl.modthespire.lib.Matcher;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertLocator;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.dungeons.Exordium;
 import com.megacrit.cardcrawl.dungeons.TheCity;
 import com.megacrit.cardcrawl.events.city.Vampires;
 import com.megacrit.cardcrawl.events.exordium.Sssserpent;
+import com.megacrit.cardcrawl.random.Random;
 
+import java.util.ArrayList;
+
+@SpirePatch(clz = AbstractDungeon.class, method = "getEvent")
 public class BlackbeardExclusiveEventsPatches {
 
-    @SpirePatch(clz = Exordium.class, method = "initializeEventList")
-    public static class InitializeEventListExordium {
-        public static void Postfix(AbstractDungeon dungeon) {
-            if (AbstractDungeon.player.chosenClass == TheBlackbeardEnum.BLACKBEARD_CLASS) {
-                AbstractDungeon.eventList.removeIf(d -> d.equals(Sssserpent.ID));
-            }
+    @SpireInsertPatch(locator = Locator.class, localvars = {"tmp"})
+    public static void Insert(Random rng, ArrayList<String> tmp) {
+        if (AbstractDungeon.player.chosenClass == TheBlackbeardEnum.BLACKBEARD_CLASS) {
+            tmp.removeIf(d -> d.equals(Sssserpent.ID));
+            tmp.removeIf(d -> d.equals(Vampires.ID));
+        } else {
+            tmp.removeIf(d -> d.equals(SsssserpentBlackbeardEvent.ID));
+            tmp.removeIf(d -> d.equals(VampiresBlackbeardEvent.ID));
         }
     }
 
-    @SpirePatch(clz = TheCity.class, method = "initializeEventList")
-    public static class InitializeEventListTheCity {
-        public static void Postfix(AbstractDungeon dungeon) {
-            if (AbstractDungeon.player.chosenClass == TheBlackbeardEnum.BLACKBEARD_CLASS) {
-                AbstractDungeon.eventList.removeIf(d -> d.equals(Vampires.ID));
-            }
+    private static class Locator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+            Matcher matcher = new Matcher.MethodCallMatcher(ArrayList.class, "isEmpty");
+            return LineFinder.findInOrder(ctMethodToPatch, matcher);
         }
     }
 
