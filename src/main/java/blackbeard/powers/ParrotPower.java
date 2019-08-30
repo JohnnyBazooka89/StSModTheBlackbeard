e56b29957713adff6a43841843197e55dc4e54a0
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2019-08-30 15:40:27 +0200

    v.1.11.1 - Parrot model.

--- src/main/java/blackbeard/powers/ParrotPower.java
+++ src/main/java/blackbeard/powers/ParrotPower.java
@@ -1,15 +1,24 @@
 package blackbeard.powers;
 
+import basemod.ReflectionHacks;
 import blackbeard.TheBlackbeardMod;
-
+import blackbeard.enums.PlayerClassEnum;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+import com.esotericsoftware.spine.Skeleton;
+import com.esotericsoftware.spine.Skin;
+import com.esotericsoftware.spine.Slot;
+import com.esotericsoftware.spine.SlotData;
+import com.esotericsoftware.spine.attachments.RegionAttachment;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -22,6 +31,8 @@ public class ParrotPower extends AbstractPower {
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
     private static final Texture power128Image = ImageMaster.loadImage(TheBlackbeardMod.getPower128ImagePath(POWER_ID));
     private static final Texture power48Image = ImageMaster.loadImage(TheBlackbeardMod.getPower48ImagePath(POWER_ID));
+
+    private static final String BLACKBEARD_PARROT_PATH = "blackbeard/char/blackbeard/idle/Parrot.png";
 
     public CardQueueItem lastCardQueueItem = null;
 
@@ -41,7 +52,7 @@ public class ParrotPower extends AbstractPower {
     }
 
     @Override
-    public void atStartOfTurn(){
+    public void atStartOfTurn() {
         lastCardQueueItem = null;
     }
 
@@ -52,14 +63,14 @@ public class ParrotPower extends AbstractPower {
         if (!card.purgeOnUse) {
             AbstractMonster m = null;
             if (action.target != null) {
-                m = (AbstractMonster)action.target;
+                m = (AbstractMonster) action.target;
             }
 
             AbstractCard tmp = card.makeSameInstanceOf();
             tmp.current_x = card.current_x;
             tmp.current_y = card.current_y;
             tmp.target_x = (float) Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
-            tmp.target_y = (float)Settings.HEIGHT / 2.0F;
+            tmp.target_y = (float) Settings.HEIGHT / 2.0F;
             tmp.freeToPlayOnce = true;
             if (m != null) {
                 tmp.calculateCardDamage(m);
@@ -68,5 +79,50 @@ public class ParrotPower extends AbstractPower {
             tmp.purgeOnUse = true;
             lastCardQueueItem = new CardQueueItem(tmp, m, card.energyOnUse);
         }
+    }
+
+    @Override
+    public void onInitialApplication() {
+        if (AbstractDungeon.player.chosenClass == PlayerClassEnum.BLACKBEARD_CLASS) {
+            Skeleton skeleton = (Skeleton) ReflectionHacks.getPrivate(AbstractDungeon.player, AbstractCreature.class, "skeleton");
+            String attachName = "Parrot";
+            String attachCloneName = "ParrotClone";
+            int origSlotIndex = skeleton.findSlotIndex(attachName);
+
+            // Create a new slot for the attachment
+            Slot origSlot = skeleton.findSlot(attachName);
+            Slot slotClone = new Slot(new SlotData(origSlot.getData().getIndex(), attachCloneName, origSlot.getBone().getData()), origSlot.getBone());
+            slotClone.getData().setBlendMode(origSlot.getData().getBlendMode());
+            skeleton.getSlots().insert(origSlotIndex, slotClone);
+
+            // Add new slot to draw order
+            Array<Slot> drawOrder = skeleton.getDrawOrder();
+            drawOrder.add(slotClone);
+            skeleton.setDrawOrder(drawOrder);
+
+            Texture tex = ImageMaster.loadImage(BLACKBEARD_PARROT_PATH);
+            tex.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+            TextureRegion region = new TextureRegion(tex);
+            RegionAttachment attachment = new RegionAttachment("ParrotClone");
+            attachment.setRegion(region);
+            attachment.setWidth(tex.getWidth());
+            attachment.setHeight(tex.getHeight());
+            attachment.setX(0 * Settings.scale);
+            attachment.setY(0 * Settings.scale);
+            attachment.setScaleX(1 * Settings.scale);
+            attachment.setScaleY(1 * Settings.scale);
+            attachment.setRotation(0.0f);
+            attachment.updateOffset();
+
+            Skin skin = skeleton.getData().getDefaultSkin();
+            skin.addAttachment(origSlotIndex, attachment.getName(), attachment);
+
+            skeleton.setAttachment(attachCloneName, attachment.getName());
+        }
+    }
+
+    @Override
+    public void onRemove() {
+
     }
 }
