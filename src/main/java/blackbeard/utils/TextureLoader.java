34a3be37cf7af6972cfe0395acf879dc2bc263ea
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2019-09-16 00:21:45 +0200

    v.1.11.21 - Change ImageMaster.loadImage to TextureLoader.getTexture.

--- src/main/java/blackbeard/utils/TextureLoader.java
+++ src/main/java/blackbeard/utils/TextureLoader.java
@@ -2,26 +2,26 @@ package blackbeard.utils;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 
 public class TextureLoader {
 
-    public static final Logger logger = LogManager.getLogger(TextureLoader.class);
     private static HashMap<String, Texture> textures = new HashMap<>();
 
-    public static Texture getTexture(final String textureString) {
-        if (!textures.containsKey(textureString)) {
-            loadTexture(textureString);
+    public static Texture getTexture(final String texturePath) {
+        Texture texture = textures.get(texturePath);
+        if (texture != null) {
+            return texture;
+        } else {
+            return loadTexture(texturePath);
         }
-        return textures.get(textureString);
     }
 
-    private static void loadTexture(final String textureString) {
-        Texture texture = new Texture(textureString);
+    private static Texture loadTexture(final String texturePath) {
+        Texture texture = new Texture(texturePath);
         texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-        textures.put(textureString, texture);
+        textures.put(texturePath, texture);
+        return texture;
     }
 }
