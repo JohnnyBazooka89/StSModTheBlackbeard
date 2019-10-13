5094db59606ed8a04f92844f299992cf4cc48099
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2019-10-13 16:09:03 +0200

    v.1.12.0 - Fixed special cards in library.

--- src/main/java/blackbeard/patches/DoNotShowSpecialCardsInLibraryPatch.java
+++ src/main/java/blackbeard/patches/DoNotShowSpecialCardsInLibraryPatch.java
@@ -1,17 +1,14 @@
 package blackbeard.patches;
 
+import basemod.patches.com.megacrit.cardcrawl.screens.compendium.CardLibraryScreen.EverythingFix;
 import blackbeard.enums.CardColorEnum;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.helpers.CardLibrary;
 
-import java.util.ArrayList;
-
-@SpirePatch(clz = CardLibrary.class, method = "getCardList", paramtypez = {CardLibrary.LibraryType.class})
+@SpirePatch(clz = EverythingFix.Initialize.class, method = "Insert", paramtypez = {Object.class})
 public class DoNotShowSpecialCardsInLibraryPatch {
 
-    public static ArrayList<AbstractCard> Postfix(ArrayList<AbstractCard> retVal, CardLibrary.LibraryType type) {
-        retVal.removeIf(c -> c.color == CardColorEnum.BLACKBEARD_BLACK && c.rarity == AbstractCard.CardRarity.SPECIAL);
-        return retVal;
+    public static void Postfix(Object object) {
+        EverythingFix.Fields.cardGroupMap.get(CardColorEnum.BLACKBEARD_BLACK).group.removeIf(c -> c.rarity == AbstractCard.CardRarity.SPECIAL);
     }
 }
