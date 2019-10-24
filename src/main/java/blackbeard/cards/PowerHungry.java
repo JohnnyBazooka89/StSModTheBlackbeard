0e6fa709c10a212760abfbf66fde08acc6a8781e
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2019-10-24 02:30:03 +0200

    v.1.12.0 - 'Power-hungry' reworked.

--- src/main/java/blackbeard/cards/PowerHungry.java
+++ src/main/java/blackbeard/cards/PowerHungry.java
@@ -16,24 +16,28 @@ public class PowerHungry extends AbstractBlackbeardCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 0;
+    private static final int CARDS_TO_PUT = 1;
+    private static final int UPGRADED_PLUS_CARDS_TO_PUT = 1;
 
     public PowerHungry() {
         super(ID, NAME, TheBlackbeardMod.getCardImagePath(ID), COST, DESCRIPTION, CardType.SKILL,
-                CardColorEnum.BLACKBEARD_BLACK, CardRarity.UNCOMMON, CardTarget.SELF);
+                CardColorEnum.BLACKBEARD_BLACK, CardRarity.RARE, CardTarget.SELF);
 
+        this.baseMagicNumber = this.magicNumber = CARDS_TO_PUT;
+        this.isInnate = true;
         this.exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new PowerCardFromDrawPileToHandAction(1));
+        this.addToBot(new PowerCardFromDrawPileToHandAction(this.magicNumber));
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.isInnate = true;
+            this.upgradeMagicNumber(UPGRADED_PLUS_CARDS_TO_PUT);
             this.rawDescription = UPGRADE_DESCRIPTION;
             this.initializeDescription();
         }
