a7e4993937eb3327d1bacf5c4c7c7d362b6b1d32
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2019-11-03 04:05:09 +0100

    v.1.20.0 - Changed logic for fixing the long description of the starter relic.

--- src/main/java/blackbeard/patches/StarterRelicFixLongDescriptionPatch.java
+++ src/main/java/blackbeard/patches/StarterRelicFixLongDescriptionPatch.java
@@ -1,14 +1,15 @@
 package blackbeard.patches;
 
 import blackbeard.enums.PlayerClassEnum;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.screens.charSelect.CharacterOption;
 
-@SpirePatch(clz = CharacterOption.class, method = "renderInfo")
+@SpirePatch(clz = CharacterOption.class, method = "renderInfo", paramtypez = {SpriteBatch.class})
 public class StarterRelicFixLongDescriptionPatch {
 
-    public static SpireReturn Prefix(CharacterOption characterOption) {
+    public static SpireReturn Prefix(CharacterOption characterOption, SpriteBatch sb) {
         if (!characterOption.selected && characterOption.c.chosenClass == PlayerClassEnum.BLACKBEARD_CLASS) {
             return SpireReturn.Return(null);
         }
