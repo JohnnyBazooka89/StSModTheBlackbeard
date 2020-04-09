3d0905ae0726e2ed3eb90e15aa6986b4d742457e
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2020-04-09 22:03:35 +0200

    v.1.15.4 - Changed art for 2 cards. Small balance change. Added googly eye for character model.

--- src/main/java/blackbeard/cards/DeadlyArsenal.java
+++ src/main/java/blackbeard/cards/DeadlyArsenal.java
@@ -17,7 +17,7 @@ public class DeadlyArsenal extends AbstractBlackbeardCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int BLOCK_AMOUNT = 6;
+    private static final int BLOCK_AMOUNT = 7;
     private static final int UPGRADE_PLUS_BLOCK_AMOUNT = 3;
 
     public DeadlyArsenal() {
