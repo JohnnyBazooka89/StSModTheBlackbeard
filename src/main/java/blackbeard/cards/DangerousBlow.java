f0719ab1d8ecd802d06e67e4c61809e7674353a1
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2020-04-15 16:49:51 +0200

    v.1.15.5 - Slightly nerfed 'Dangerous Blow'.

--- src/main/java/blackbeard/cards/DangerousBlow.java
+++ src/main/java/blackbeard/cards/DangerousBlow.java
@@ -19,7 +19,7 @@ public class DangerousBlow extends AbstractBlackbeardCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int ATTACK_DMG = 18;
+    private static final int ATTACK_DMG = 16;
     private static final int UPGRADE_PLUS_ATTACK_DMG = 4;
     private static final int DAMAGE_TO_TAKE = 4;
 
