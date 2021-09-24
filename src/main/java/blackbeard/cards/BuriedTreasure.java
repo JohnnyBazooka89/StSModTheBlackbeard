534983156900ef2b2f5e0d495cef86fb8b664daa
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2021-09-24 20:52:10 +0200

    v.1.16.7 - Nerfed gold gaining cards.

--- src/main/java/blackbeard/cards/BuriedTreasure.java
+++ src/main/java/blackbeard/cards/BuriedTreasure.java
@@ -13,7 +13,7 @@ public class BuriedTreasure extends AbstractBlackbeardCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 0;
+    private static final int COST = 1;
     private static final int GOLD_TO_GAIN = 15;
     private static final int UPGRADE_PLUS_GOLD_TO_GAIN = 5;
 
